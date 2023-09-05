27876d2cc0662ca9b3bd90310c1fbe78b8169516
Nathaniel Woodland <nathanielwoodland@gmail.com>
2023-09-05 17:39:26 -0400

    Add support for refreshing existing property nodes.

--- src/Controller/LodgifyController.php
+++ src/Controller/LodgifyController.php
@@ -5,11 +5,13 @@ namespace Drupal\lodgify\Controller;
 use Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException;
 use Drupal\Component\Plugin\Exception\PluginNotFoundException;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\EntityInterface;
 use Drupal\Core\Entity\EntityStorageException;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\file\FileRepositoryInterface;
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Client;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -60,6 +62,10 @@ final class LodgifyController extends ControllerBase {
     return $build;
   }
 
+  /**
+   * @return array
+   * @throws \GuzzleHttp\Exception\GuzzleException
+   */
   private function getProperties(): array {
     $guzzle_client_options = $this->getGuzzleClientOptions();
     $client = new Client();
@@ -68,33 +74,63 @@ final class LodgifyController extends ControllerBase {
     $lodgify_properties = json_decode($response->getBody()->getContents());
     return $lodgify_properties->items;
   }
-  public function refreshProperties() {
+
+  /**
+   * @param int $property_id
+   *
+   * @return \Drupal\Core\Entity\EntityInterface
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   */
+  private function getOrCreatePropertyById(int $property_id): EntityInterface {
+    $lodgify_property_search = $this->entityTypeManager()
+      ->getStorage('node')
+      ->getQuery()
+      ->accessCheck(TRUE)
+      ->condition('field_lodgify_id', $property_id)
+      ->execute();
+    if ($lodgify_property_search) {
+      return $this->entityTypeManager()
+        ->getStorage('node')
+        ->load(reset($lodgify_property_search));
+    } else {
+      return $this->entityTypeManager()
+        ->getStorage('node')
+        ->create([
+          'type' => 'lodgify_property',
+        ]);
+    }
+  }
+
+  /**
+   * @return \Symfony\Component\HttpFoundation\RedirectResponse
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * @throws \Drupal\Core\Entity\EntityStorageException
+   * @throws \GuzzleHttp\Exception\GuzzleException
+   */
+  public function refreshProperties(): RedirectResponse {
     $lodgify_properties = $this->getProperties();
     foreach ($lodgify_properties as $key => $value) {
-      // @todo: Check if property already exists
       $property_id = $lodgify_properties[$key]->id;
-
-      // Create file object from remote URL.
+      $lodgify_property_node = $this->getOrCreatePropertyById($property_id);
+      // Create image file from API.
       $image_url = 'https:' . $lodgify_properties[$key]->image_url;
       $image_data = file_get_contents($image_url);
-      $image_file = $this->fileRepository->writeData($image_data, "public://$property_id.cover_image.png");
-
-      // Create node object with attached file.
-      $node = $this->entityTypeManager()->getStorage('node')->create([
-        'type' => 'lodgify_property',
-        'title' => $lodgify_properties[$key]->name,
-        'field_lodgify_description' => $lodgify_properties[$key]->description,
-        'field_lodgify_id' => $property_id,
-        'field_lodgify_cover_image' => [
+      $image_file = $this->fileRepository
+        ->writeData($image_data, "public://$property_id.cover_image.png");
+      // Create or update node from API data.
+      $lodgify_property_node->set('title', $lodgify_properties[$key]->name);
+      $lodgify_property_node->set('field_lodgify_description', $lodgify_properties[$key]->description);
+      $lodgify_property_node->set('field_lodgify_id', $property_id);
+      $lodgify_property_node->set('field_lodgify_cover_image', [
           'target_id' => $image_file->id(),
           'alt' => 'Lodgify Property',
           'title' => 'Lodgify Property'
-        ],
-      ]);
-      $node->save();
+        ]);
+      $lodgify_property_node->save();
     }
-
-    return $this->propertiesListPage();
+    return $this->redirect('lodgify.view_properties');
   }
 
 }
