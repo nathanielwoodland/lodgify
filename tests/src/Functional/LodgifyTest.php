5283b62accef4d6792b7032505d2f3f8476a562e
Nathaniel Woodland <nathanielwoodland@gmail.com>
2023-09-07 18:00:18 -0400

    Add functional test coverage for data sync routes

--- tests/src/Functional/LodgifyTest.php
+++ tests/src/Functional/LodgifyTest.php
@@ -35,28 +35,59 @@ final class LodgifyTest extends BrowserTestBase {
    */
   protected function setUp(): void {
     parent::setUp();
-    // Set up the test here.
+    // @todo mock API methods to test without key and live integration
+    $this->config('lodgify.settings')->set('api_key', 'YXjlubOq1Y4ocAeqS04g6VNWJY3iyg+XsVVoj0TkLXH10ZdNVWQZk0UMzTAVre7n')->save();
   }
 
   /**
-   * Test callback.
+   * Test properties listing page.
    */
   public function testPropertiesListingPage(): void {
     $user = $this->drupalCreateUser(['access content']);
     $this->drupalLogin($user);
-    $this->drupalGet('lodgify-properties');
-    $this->assertSession()->elementExists('xpath', '//h1[text() = "Lodgify properties"]');
+    $this->drupalGet('lodgify/properties');
+    $this->assertSession()->elementTextEquals('css', 'h1.page-title', 'Lodgify properties');
   }
 
-//  public function testSyncNewProperty(): void {
-//    $lodgify_mock_class = $this
-//      ->createPartialMock() ('Drupal\lodgify\Controller\LodgifyController')
-//      ->disableOriginalConstructor()
-//      ->onlyMethods('getProperties')
-//      ->getMock();
-//  }
+  /**
+   * Test sync new properties.
+   */
+  public function testSyncNewProperties(): void {
+    $user = $this->drupalCreateUser(['administer site configuration']);
+    $this->drupalLogin($user);
+    $new_record_sync_types = ['all', 'new'];
+    foreach ($new_record_sync_types as $new_record_sync_type) {
+      $this->drupalGet("/lodgify/sync-properties/$new_record_sync_type");
+      $this->drupalGet('/lodgify/properties');
+      $this->assertSession()
+        ->elementTextEquals('css', 'td.views-field-title', 'Huge condo in Keystone, sleeps 14, great location');
+      $this->drupalGetNodeByTitle('Huge condo in Keystone, sleeps 14, great location')->set('title', 'Tiny condo in Keystone')->delete();
+    }
+  }
 
-//  public function testSyncExistingProperty(): void {
-//  }
+  /**
+   * Test sync existing properties.
+   */
+  public function testSyncExistingProperties(): void {
+    $user = $this->drupalCreateUser(['administer site configuration']);
+    $this->drupalLogin($user);
+    $this->drupalGet('/lodgify/sync-properties/all');
+    $this->drupalGet('/lodgify/properties');
+    $this->assertSession()
+      ->elementTextEquals('css', 'td.views-field-title', 'Huge condo in Keystone, sleeps 14, great location');
+    $new_record_sync_types = ['all', 'existing'];
+    foreach ($new_record_sync_types as $new_record_sync_type) {
+      $this->drupalGetNodeByTitle('Huge condo in Keystone, sleeps 14, great location')
+        ->set('title', 'Tiny condo in Keystone')
+        ->save();
+      $this->drupalGet('/lodgify/properties');
+      $this->assertSession()
+        ->elementTextEquals('css', 'td.views-field-title', 'Tiny condo in Keystone');
+      $this->drupalGet("/lodgify/sync-properties/$new_record_sync_type");
+      $this->drupalGet('/lodgify/properties');
+      $this->assertSession()
+        ->elementTextEquals('css', 'td.views-field-title', 'Huge condo in Keystone, sleeps 14, great location');
+    }
+  }
 
 }
