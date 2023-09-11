468be72dd17546b68b9ebe3da4e4c3aafb2dc108
Nathaniel Woodland <nathanielwoodland@gmail.com>
2023-09-11 16:17:40 -0400

    refactor: Implement getLodgifyData using Drupal core httpClient service

--- src/LodgifyApiClient.php
+++ src/LodgifyApiClient.php
@@ -15,6 +15,7 @@ final class LodgifyApiClient {
    */
   public function __construct(
     private readonly ConfigFactoryInterface $configFactory,
+    private readonly Client $httpClient,
   ) {}
 
   /**
@@ -27,11 +28,9 @@ final class LodgifyApiClient {
    * @throws \GuzzleHttp\Exception\GuzzleException
    */
   public function getLodgifyData(string $record_type, string $query_params): array {
-    $client = new Client();
     // @todo: add pagination support for more than 50 results
-    // @todo: use Drupal HTTP client instead of Guzzle
     $page_number = 1;
-    $response = $client->request('GET', "https://api.lodgify.com/v2/$record_type?includeCount=true&page=$page_number&size=50$query_params", $this->getGuzzleRequestHeaders());
+    $response = $this->httpClient->request('GET', "https://api.lodgify.com/v2/$record_type?includeCount=true&page=$page_number&size=50$query_params", $this->getGuzzleRequestHeaders());
     return json_decode($response->getBody()->getContents())->items;
   }
 
