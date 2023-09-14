79a16bfc49c3e28d3ce82b774f0dc8c06adc0458
Nathaniel Woodland <nathanielwoodland@gmail.com>
2023-09-14 13:03:35 -0400

    refactor: remove unneeded services

--- src/Controller/PropertiesController.php
+++ src/Controller/PropertiesController.php
@@ -3,34 +3,8 @@
 namespace Drupal\lodgify\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Drupal\lodgify\PropertiesSyncService;
 
 final class PropertiesController extends ControllerBase {
-
-  /**
-   * @var \Drupal\lodgify\PropertiesSyncService
-   */
-  protected PropertiesSyncService $propertiesSyncService;
-
-  /**
-   * The controller constructor.
-   */
-  public function __construct(
-    PropertiesSyncService $propertiesSyncService,
-  ) {
-    $this->propertiesSyncService = $propertiesSyncService;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container): self {
-    return new self(
-      $container->get('lodgify.properties_sync_service'),
-    );
-  }
 
   /**
    * Returns the Lodgify properties listing view.
