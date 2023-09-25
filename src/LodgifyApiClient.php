d02e7c26b390f8d3929307b197631ac86bbb69e9
Nathaniel Woodland <nathanielwoodland@gmail.com>
2023-09-25 14:24:49 -0400

    chore: fix phpcs issues

--- src/LodgifyApiClient.php
+++ src/LodgifyApiClient.php
@@ -33,9 +33,18 @@ final class LodgifyApiClient {
    * Gets data from Lodgify API.
    *
    * @param string $route
+   *   The Lodgify V2 API route.
+   *     - see: https://docs.lodgify.com/reference/getallpropertiesasync.
    * @param string $query_params
+   *   The Lodgify API Query Params.
+   *     - see https://docs.lodgify.com/reference/getallpropertiesasync.
    *
    * @return array
+   *   Returns array in following format:
+   *     [
+   *       'success' => TRUE|FALSE,
+   *       'records' => array, // Only if success = TRUE
+   *     ]
    */
   public function getLodgifyData(string $route, string $query_params): array {
     $headers = $this->getRequestHeaders();
@@ -53,7 +62,7 @@ final class LodgifyApiClient {
     }
     catch (GuzzleException $e) {
       $error_code = $e->getCode();
-      $error_message = $this->t("API request for Lodgify $route records failed with HTTP status code: $error_code.");
+      $error_message = $this->t('API request for Lodgify') . " $route " . $this->t('records failed with HTTP status code') . ": $error_code.";
       $this->messenger->addError($error_message);
       $this->logger->error($error_message);
       return [
@@ -62,7 +71,7 @@ final class LodgifyApiClient {
     }
     $response = json_decode($response->getBody()->getContents(), TRUE);
     if (empty($response['items'])) {
-      $error_message = $this->t("No $route records found.");
+      $error_message = $this->t('No records found of type:') . " $route";
       $this->messenger->addError($error_message);
       $this->logger->error($error_message);
       return [
@@ -78,7 +87,8 @@ final class LodgifyApiClient {
   /**
    * Gets request headers including API key for authentication.
    *
-   * @return bool|array[]
+   * @return false|array[]
+   *   Either returns the request headers array, or FALSE.
    */
   private function getRequestHeaders(): array|bool {
     $api_key = $this->settings->get('lodgify_api_key');
